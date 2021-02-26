43c6f2205efcd0548f1dd3a6ff87794290c6fa48
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-02-26 19:32:11 +0530

    feat: add `nuxt-vite`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,6 +46,8 @@ export default {
     '@nuxtjs/tailwindcss',
     // https://color-mode.nuxtjs.org/#setup
     '@nuxtjs/color-mode',
+    // https://github.com/nuxt/vite
+    'nuxt-vite',
   ],
   // Tailwind Config
   tailwindcss: {
