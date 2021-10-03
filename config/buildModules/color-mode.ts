ed890a9379f950e487f2cfa0f0513b683a5a388f
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2021-10-04 02:00:43 +0530

    chore: remove tailwind css classes

--- config/buildModules/color-mode.ts
+++
@@ -1,8 +0,0 @@
-import { ColorModeConfig } from '@nuxtjs/color-mode/types/color-mode';
-
-const config: ColorModeConfig = {
-  preference: 'dark',
-  classSuffix: '',
-};
-
-export { config as colorMode };
