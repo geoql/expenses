6f99705ee9808e52359624d03222278b2f47eb71
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-03-09 18:14:25 +0530

    feat: add typings for modules
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- config/modules/index.ts
+++ config/modules/index.ts
@@ -1,4 +1,6 @@
-const modules: any[] = [
+import type { NuxtConfig } from 'nuxt/schema';
+
+const modules: NuxtConfig['modules'] = [
   // https://pinia.esm.dev/ssr/nuxt.html#nuxt-js
   '@pinia/nuxt',
   // https://windicss.org/guide/
