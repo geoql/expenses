ac33a196f90dbded1e5a6b04a43bec3c65545981
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-03-14 13:32:57 +0530

    chore: dependabot config clean up
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- ship.config.cjs
+++ ship.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  installCommand: () => 'npm i --prefer-offline --no-audit --no-optional',
+  installCommand: () => 'npm i --prefer-offline --no-audit --omit=optional',
   buildCommand: () => null,
   publishCommand: ({ tag }) =>
     `echo "Releasing ${tag} version of Expenses frontend webapp"`,
