db993b30497d5f860666a4661396f75260ce4aa0
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-03-20 23:56:09 +0530

    feat(seo): add opengraph tags 📁
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- config/head.ts
+++ config/head.ts
@@ -11,13 +11,30 @@ const head: NuxtAppConfig['head'] = {
       content: 'width=device-width, initial-scale=1',
     },
     {
+      name: 'format-detection',
+      content: 'telephone=no',
+    },
+    { hid: 'title', name: 'title', content: 'Map My Expenses' },
+    {
       hid: 'description',
       name: 'description',
+      content: 'Visualize all your expenses on the map',
+    },
+    { hid: 'og:title', property: 'og:title', content: 'Map My Expenses.' },
+    {
+      hid: 'og:description',
+      property: 'og:title',
       content: 'Quickly map all your expenses',
     },
     {
-      name: 'format-detection',
-      content: 'telephone=no',
+      hid: 'og:site_name',
+      property: 'og:site_name',
+      content: 'Map My Expenses',
+    },
+    {
+      hid: 'twitter:description',
+      property: 'og:site_name',
+      content: 'Quickly map all your expenses',
     },
   ],
   link: [
