ee540242bcafa06e44631fa824c18dd636639017
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-04-20 02:07:22 +0530

    chore: update eslint config 🕺
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,9 @@ module.exports = {
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    ecmaVersion: 2020,
+    ecmaVersion: 2022,
     sourceType: 'module',
-    lib: ['es2020'],
+    lib: ['es2022'],
     ecmaFeatures: {
       jsx: true,
       tsx: true,
