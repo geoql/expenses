ba4ee54e81e8dbc45e736c8f239aa062f236d7fb
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2024-03-09 22:12:59 +0530

    build: add pipeline ⛏️

--- ship.config.cjs
+++ ship.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  installCommand: () => 'npm i --prefer-offline --no-audit --omit=optional',
+  installCommand: () => 'npm i --prefer-offline --no-audit',
   buildCommand: () => null,
   publishCommand: ({ tag }) =>
     `echo "Releasing ${tag} version of Expenses frontend webapp"`,
